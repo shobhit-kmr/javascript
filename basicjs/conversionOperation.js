356cdaefcac45039f5273ee677a4f4a5a341b46e
shobhit-kmr <114897185+shobhit-kmr@users.noreply.github.com>
2023-10-06 18:15:38 +0000

    conversion

---
+++ basicjs/conversionOperation.js
@@ -0,0 +1,15 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+//
