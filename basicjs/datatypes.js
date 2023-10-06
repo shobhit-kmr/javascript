08dde35f8683d3f80626a5fd16bb300ac35cd987
shobhit-kmr <114897185+shobhit-kmr@users.noreply.github.com>
2023-10-06 18:02:13 +0000

    datatypes

---
+++ basicjs/datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as newer versiion
+
+// alert(3 + 3)// we are using nodejs, not browser
+
+let name = "boby"
+let age = 18
+let isLoggedIn = false
+
+// number => "boby"
+// bigint
+// string => ""
+// boolea => true/false
+// null => standalone
+// undefined =>
+// symbol => unique 
+
+
+//object
+
+console.log(typeof undefined); // type undefined
+console.log(typeof null); // type object
