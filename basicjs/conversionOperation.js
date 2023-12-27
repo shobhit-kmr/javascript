ccb540c5e6938b0fa56b086d9e263e5b54d27f14
shobhit-kmr <114897185+shobhit-kmr@users.noreply.github.com>
2023-12-27 10:58:00 +0000

    conversion of datatypes

--- basicjs/conversionOperation.js
+++ basicjs/conversionOperation.js
@@ -1,11 +1,15 @@
-let score = null
+let score1 = 33 // type is number
+let score2= "33" // typeof is string
+let score3 = "33ABC" //typeof is number but value is NaN
+let score4 = undefined // typeof is NaN
+let score5 = null //typeof is undefined
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 
@@ -13,3 +17,12 @@ console.log(valueInNumber);
 // "33abc" => NaN
 // true => 1; false =>0
 //
+
+
+let isLoggin = 1
+let booleanLoggedIn = Boolean(isLoggin)
+console.log(booleanLoggedIn)
+
+// 1=> true, 0=> false
+// "" => false, 
+// "shobhit" => true
