ee2be8d761b4c07bcf83645b6fc41d052b33f664
shobhit-kmr <114897185+shobhit-kmr@users.noreply.github.com>
2024-01-02 13:02:41 +0000

    added comment

--- basicjs/datatypes-summary.js
+++ basicjs/datatypes-summary.js
@@ -35,3 +35,7 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// memmory allocation
+
+// stack (primitive),   heap(non-primitive)
