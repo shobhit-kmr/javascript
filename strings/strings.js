b9129bcd0a6d184f2f7f7e772487adc6f9becfdc
shobhit-kmr <114897185+shobhit-kmr@users.noreply.github.com>
2024-01-04 07:45:49 +0000

    added strings.js

---
+++ strings/strings.js
@@ -0,0 +1,34 @@
+const name = 'shobhit kumar'
+const repoCount = 17
+
+// console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+// another way to define strings 
+
+const newStr = new String('boby swaroop')
+// console.log(newStr)
+// console.log(newStr[0])
+// console.log(newStr.__proto__)
+// console.log(newStr.length);
+// console.log(newStr.toUpperCase());
+// console.log(newStr.charAt(3));
+// console.log(newStr.indexOf("s"));
+// console.log(newStr.substring(0,5)); // its give 0-4 element not count last digit
+// console.log(newStr.slice(-12,11));
+
+const strName = "   rahul   "
+// console.log(strName)
+// console.log(strName.trim())   // to remove extra space
+
+// url rewrite using string methods
+
+const url = "https://google.com//boby%20swaroop"
+
+// console.log(url.replace('%20',"-"));// replace url to %20 to  -
+
+// console.log(url.includes("com")); // to check url strings
+
+// console.log(url.includes("rahul")); // to check url strings
+
+const nameSecond = "aftab-alam-com"
+console.log(nameSecond.split("-"));
