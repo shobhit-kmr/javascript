01a3e72ed72c90b4ede7b32ba20faf186ee45e19
shobhit-kmr <114897185+shobhit-kmr@users.noreply.github.com>
2024-01-06 18:18:26 +0000

    added

--- basicjs/num-and-math.js
+++ basicjs/num-and-math.js
@@ -14,6 +14,8 @@ const otherNumber = 123.8966
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
 
+
+
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
@@ -23,12 +25,5 @@ const hundreds = 1000000
 // console.log(Math.floor(4.9));
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
-
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
-
-const min = 10
-const max = 20
-
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.max(4, 3, 6, 8));
+console.log(typeof(balance))
